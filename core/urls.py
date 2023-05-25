9f5d19208a8c83c988db199a7504b7874a08e14c
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-25 09:54:35 +0500

    done login

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth.models import User
+
+
+class LoginHome(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+    
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ClassRoom.urls')),
     path('lesson/', include('LessonPlanner.urls')),
-    path('payment/', include('PayMents.urls'))
+    path('payment/', include('PayMents.urls')),
+    path('login/', LoginHome.as_view()),
 ]
