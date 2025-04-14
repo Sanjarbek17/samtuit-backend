e8892f12aeae48b1215b81fcd8b26ae6ac45cf12
Sanjarbek17 <sanjarsaidov173@gmail.com>
2025-04-14 10:31:23 +0500

    git add media url

--- core/settings.py
+++ core/settings.py
@@ -133,6 +133,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static/'
 
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
